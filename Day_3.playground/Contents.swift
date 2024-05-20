97b8ea1979cf12467d1874a2f60e973c0ff87380
Amit Kundu <kundu.amit517@gmail.com>
2024-05-20 23:15:00 +0300

    Day 4 added

--- Day_3.playground/Contents.swift
+++ Day_3.playground/Contents.swift
@@ -1,3 +1,101 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+let firstScore = 12
+let secondScore = 4
+
+
+
+let sum = firstScore + secondScore
+print(sum)
+
+let weeks = 465 / 7
+
+print("There are \(weeks) weeks until the event.")
+
+let weeks_d: Double = 465 / 7
+
+print("There are \(weeks_d) weeks until the event.")
+
+let number = 465
+let isMultiple = number.isMultiple(of: 7)
+
+
+let meaningOfLife = 42
+let doubleMeaning = 42
+
+let fakers = "Fakers gonna "
+let action = fakers + "fake"
+
+let firstHalf = ["John", "Paul"]
+let secondHalf = ["Amit", "Kundu"]
+let beatles = firstHalf + secondHalf
+
+var score = 95
+score -= 5
+
+var quote = "The rain in Spain falles mainly on the "
+quote += "Spaniards"
+
+firstScore > secondScore
+firstScore == secondScore
+
+firstScore >= secondScore
+firstScore != secondScore
+
+"Taylor" <= "Swift"
+
+
+let firstCard = 11
+let secondCard = 10
+
+if firstCard + secondCard == 2 {
+    print("Aces – lucky!")
+} else if firstCard + secondCard == 21 {
+    print("Blackjack!")
+} else {
+    print("Regular cards")
+}
+
+
+let age1 = 12
+let age2 = 21
+
+if age1 > 18 && age2 > 18 {
+    print("Both age over 18")
+}
+
+if age1 > 18 || age2 > 18 {
+    print("At least one age over 18")
+}
+
+print(firstCard == secondCard ? "Cards are same" : "Cards are not same")
+
+
+let weather = "sunny"
+
+switch weather {
+case "rain":
+    print("Bring an umbrella")
+case "snow":
+    print("Wrap up warm")
+case "sunny":
+    print("Wear sunscreen")
+default:
+    print("Enjoy your day")
+}
+
+switch score{
+case 0..<50:
+    print("You failed badly")
+case 0..<85:
+    print("You did OK.")
+default:
+    print("You did great!")
+}
+
+let names = ["Piper", "Alex", "Suzanne", "Gloria"]
+print(names[0...])
+print(names[0])
+
--- Day_4.playground/Contents.swift
+++ Day_4.playground/Contents.swift
@@ -1,3 +1,77 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+
+var number = 1
+
+while number <= 20 {
+    print(number)
+    number += 1
+}
+
+print("Ready or not here I come")
+
+
+repeat {
+    print(number)
+} while number <= 20
+
+print("Ready or not here I come")
+
+
+while false {
+    print("This is false")
+}
+
+repeat {
+    print("This is false")
+} while false
+
+
+var countDown = 10
+while countDown >= 0 {
+    print(countDown)
+
+    if countDown == 4 {
+        print("I'm bored. Let's go now!")
+        break
+    }
+
+    countDown -= 1
+}
+
+print("Blast off")
+
+outerLoop: for i in 1...10 {
+    for j in 1...10 {
+        let product = i * j
+        print("\(i) * \(j) is \(product)")
+
+        if product == 50 {
+            print("It's a bullseye!")
+            break outerLoop
+        }
+    }
+}
+
+for i in 1...10 {
+    if i % 2 == 1 {
+        print(i)
+        continue
+    }
+
+    print(i)
+}
+
+var counter = 0
+
+while true {
+    print(" ")
+
+    if counter == 273 {
+        break
+    }
+
+    counter += 1
+}
