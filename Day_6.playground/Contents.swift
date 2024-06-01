f5f1e9fe766bbda1c4eb39c9ad9f354e45381fc6
Amit Kundu <kundu.amit517@gmail.com>
2024-06-01 11:57:04 +0300

    Day 6 completed | Day 7 added

--- Day_6.playground/Contents.swift
+++ Day_6.playground/Contents.swift
@@ -1,2 +1,39 @@
 import UIKit
 
+let driving = {
+    print("I'm driving in my car")
+}
+
+driving()
+
+var flying = { (place: String) in
+    print("I'm flying to \(place)")
+}
+
+flying("London")
+
+var drivingWithReturn = { (places: String) -> String in
+    return "I'm going to \(places) in my car"
+
+}
+
+let message = drivingWithReturn("London")
+print(message)
+print(message)
+
+func travel (action: () -> Void) {
+    print("I'm getting ready to go")
+    action()
+    print("I arrived")
+}
+
+travel(action: driving)
+
+func holdClass(_ name: String, lesson: () -> Void) {
+    print("Welcome to \(name)!")
+    lesson()
+    print("Make sure your homework is done by next week.")
+}
+holdClass("Philosophy 101") {
+    print("All we are is dust in the wind, dude.")
+}
