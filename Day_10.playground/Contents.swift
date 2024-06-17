070ffe794c605ac507f5187d5494da64e6196b5b
Amit Kundu <kundu.amit517@gmail.com>
2024-06-17 20:04:28 +0300

    Day 10 complete

--- Day_10.playground/Contents.swift
+++ Day_10.playground/Contents.swift
@@ -1,3 +1,65 @@
 import UIKit
 
-var greeting = "Hello, playground"
+class Dog {
+    var name: String
+    var breed: String
+
+    init(name: String, breed: String) {
+        self.name = name
+        self.breed = breed
+    }
+
+    func makeNoise() {
+        print("Woof!")
+    }
+}
+
+let poppy = Dog(name: "Poppy", breed: "Poodle")
+
+print(poppy)
+
+class Empty { }
+let nothing = Empty()
+
+class Poodle: Dog {
+    init(name: String) {
+        super.init(name: name, breed: "Poodle")
+    }
+
+    override func makeNoise() {
+        print("Yip!")
+    }
+}
+
+let myPoodle = Poodle(name: "Jack")
+print(myPoodle.name)
+myPoodle.makeNoise()
+
+class Person {
+    var name = "John Doe"
+
+    init() {
+        print("\(name) is alive")
+    }
+
+    func printGreeting() {
+        print("Hello, I'm \(name)")
+    }
+    
+    deinit{
+        print("\(name) is no more \n....")
+    }
+}
+
+for _ in 1...3 {
+    let person = Person()
+    person.printGreeting()
+}
+
+class Singer{
+    let name = "Taylor Swift"
+}
+
+let taylor = Singer()
+print(taylor.name)
+
